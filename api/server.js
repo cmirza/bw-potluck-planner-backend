dd0603dcba410d10a9454ac983499df7861a35e7
cmirza <mail@cmirza.com>
2021-02-01 19:55:32 -0800

    dependencies and basic server

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'API Running...' });
+});
+
+module.exports = server;
